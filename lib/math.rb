3877a43d8e8f7618785c8b6c3d8d682dc1eb26df
Meredith McIntosh <meredith@mcintosh.io>
2020-09-14 20:21:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,10 +15,10 @@ addition('1','2')
 
 # end
 
-def modulo(num1, num2)
-  puts num1.modulo(num1)
-end
-modulo(5, 34)
+# def modulo(num1, num2)
+#   puts num1.modulo(num1)
+# end
+# modulo(5, 34)
 
 def square_root(num)
   return Math.sqrt(num)
