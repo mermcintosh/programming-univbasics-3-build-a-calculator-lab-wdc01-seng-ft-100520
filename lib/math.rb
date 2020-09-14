101e6044f084cffde7804b9311d52af8472efc82
Meredith McIntosh <meredith@mcintosh.io>
2020-09-14 20:35:15 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  return num1 + num2
 end
+addition(1,2)
 
 def subtraction(num1, num2)
-
+  return num1 - num2
 end
+subtraction(10,5)
 
 def division(num1, num2)
-
+  return num1 / num2
 end
+division(10,5)
 
 def multiplication(num1, num2)
-
+  return num1 * num2
 end
+multiplication(10,5)
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
+modulo(35, 4)
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
+square_root(81)
